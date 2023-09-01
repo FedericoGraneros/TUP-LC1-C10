55c3ac1ff0793b2a6958054953934c4aec9c0875
FedericoGraneros <granerosfede22@gmail.com>
2023-09-01 20:45:32 -0300

    Ejercicio 5

--- LaboratorioComputacion1/UNIDAD 3/Ejercicios/ejercicio5.c
+++ LaboratorioComputacion1/UNIDAD 3/Ejercicios/ejercicio5.c
@@ -1,1 +1,23 @@
-adfjaifajfa
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+/*Escriba un programa que pida ingresar un número y a continuación escriba en la
+consola si el mismo es par o impar.*/
+
+int main()
+{
+    int num;
+
+    printf("Ingrese un numero:");
+    scanf("%i", &num);
+
+    if (num % 2 == 0)
+    {
+        printf("El numero es par.\n");
+    }
+    else
+    {
+        printf("El numero es impar.\n");
+    }
+}
